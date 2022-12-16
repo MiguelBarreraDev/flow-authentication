67d3a7faabd1346b2d655c9ef0d02695900ee7a8
MiguelBarreraDev <barreram987@gmail.com>
2022-12-15 21:17:01 -0500

    Add: Form validation

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   ModalBody,
@@ -9,30 +10,59 @@ import {
   ModalFooter,
   ModalHeader
 } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
 
 interface LoginProps {
   onClose: () => void
 }
 
 export const Login: React.FC<LoginProps> = ({ onClose }) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm()
+
+  const onSubmit = handleSubmit(async (values) => {
+    console.log(values)
+  })
+
   return (
     <Box flex={1} display="flex" flexDir="column" pos="relative">
       <ModalHeader>Log In</ModalHeader>
       <ModalCloseButton />
       <ModalBody pb={6}>
-        <FormControl>
-          <FormLabel>First name</FormLabel>
-          <Input placeholder="First name" />
-        </FormControl>
-
-        <FormControl mt={4}>
-          <FormLabel>Last name</FormLabel>
-          <Input placeholder="Last name" />
-        </FormControl>
+        <form onSubmit={onSubmit} id="form-login" noValidate>
+          <FormControl isRequired isInvalid={Boolean(errors.username)}>
+            <FormLabel>First name</FormLabel>
+            <Input
+              {...register('username', {
+                required: 'This field is required'
+              })}
+              placeholder="Username"
+            />
+            <FormErrorMessage>
+              {errors.username?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl mt={4} isRequired isInvalid={Boolean(errors.password)}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              {...register('password', {
+                required: 'This field is required'
+              })}
+              placeholder="Password"
+            />
+            <FormErrorMessage>
+              {errors.password?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+        </form>
       </ModalBody>
 
       <ModalFooter>
-        <Button colorScheme="purple" mr={3}>
+        <Button type="submit" form="form-login" colorScheme="purple" mr={3}>
           Save
         </Button>
         <Button onClick={onClose}>Cancel</Button>
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   ModalBody,
@@ -9,30 +10,82 @@ import {
   ModalFooter,
   ModalHeader
 } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
 
 interface SignupProps {
   onClose: () => void
 }
 
 export const Signup: React.FC<SignupProps> = ({ onClose }) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm()
+
+  const onSubmit = handleSubmit(async (values) => {
+    console.log(values)
+  })
+
   return (
     <Box flex={1} display="flex" flexDir="column" pos="relative">
       <ModalHeader>Create your account</ModalHeader>
       <ModalCloseButton />
       <ModalBody pb={6}>
-        <FormControl>
-          <FormLabel>First name</FormLabel>
-          <Input placeholder="First name" />
-        </FormControl>
-
-        <FormControl mt={4}>
-          <FormLabel>Last name</FormLabel>
-          <Input placeholder="Last name" />
-        </FormControl>
+        <form onSubmit={onSubmit} id="form-signup" noValidate>
+          <FormControl isRequired isInvalid={Boolean(errors.first_name)}>
+            <FormLabel>First name</FormLabel>
+            <Input
+              {...register('first_name', {
+                required: 'This field is required'
+              })}
+              placeholder="First name"
+            />
+            <FormErrorMessage>
+              {errors.first_name?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl mt={4} isRequired isInvalid={Boolean(errors.last_name)}>
+            <FormLabel>Last name</FormLabel>
+            <Input
+              {...register('last_name', {
+                required: 'This field is required'
+              })}
+              placeholder="Last name"
+            />
+            <FormErrorMessage>
+              {errors.last_name?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl mt={4} isRequired isInvalid={Boolean(errors.username)}>
+            <FormLabel>Username</FormLabel>
+            <Input
+              {...register('username', {
+                required: 'This field is required'
+              })}
+              placeholder="Username"
+            />
+            <FormErrorMessage>
+              {errors.username?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl mt={4} isRequired isInvalid={Boolean(errors.password)}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              {...register('password', {
+                required: 'This field is required'
+              })}
+              placeholder="Password"
+            />
+            <FormErrorMessage>
+              {errors.password?.message?.toString()}
+            </FormErrorMessage>
+          </FormControl>
+        </form>
       </ModalBody>
 
       <ModalFooter>
-        <Button colorScheme="purple" mr={3}>
+        <Button type="submit" form="form-signup" colorScheme="purple" mr={3}>
           Save
         </Button>
         <Button onClick={onClose}>Cancel</Button>
