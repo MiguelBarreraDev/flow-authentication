3b33e3fecccdfad43ce7840c45107bd443096cae
MiguelBarreraDev <barreram987@gmail.com>
2022-12-23 19:36:28 -0500

    Add: Generate and validate methods for refresh token

--- backend/src/utils/jwt.utils.js
+++ backend/src/utils/jwt.utils.js
@@ -1,13 +1,27 @@
 import jsonwebtoken from 'jsonwebtoken'
 
-const { JWT_PRIVATE_KEY } = process.env
+const { JWT_PRIVATE_KEY, JWT_REFRESH_KEY } = process.env
 
-export const signJWT = ({ payload }) => {
-  const token = jsonwebtoken.sign(payload, JWT_PRIVATE_KEY, {
+export const generateToken = ({ payload }) => {
+  const expiresIn = 60 * 15
+  const accessToken = jsonwebtoken.sign(payload, JWT_PRIVATE_KEY, {
     algorithm: 'HS256',
-    expiresIn: '1d'
+    expiresIn
   })
-  return token
+  return { accessToken, expiresIn }
 }
 
-export const verifyJWT = (jwt) => jsonwebtoken.verify(jwt, JWT_PRIVATE_KEY)
+export const verifyToken = (jwt) => jsonwebtoken.verify(jwt, JWT_PRIVATE_KEY)
+
+export const generateRefreshToken = ({ payload }) => {
+  const expiresIn = 60 * 60 * 24
+  const refreshToken = jsonwebtoken.sign(payload, JWT_REFRESH_KEY, {
+    algorithm: 'HS256',
+    expiresIn
+  })
+
+  return { refreshToken, expiresIn }
+}
+
+export const verifyRefreshToken = (jwt) =>
+  jsonwebtoken.verify(jwt, JWT_REFRESH_KEY)
