d4a8b8dde046c47d1542144869b584db9b5dae55
MiguelBarreraDev <barreram987@gmail.com>
2022-12-23 19:40:10 -0500

    Update: Formated code

--- backend/src/dto/userDTO/user-login.dto.js
+++ backend/src/dto/userDTO/user-login.dto.js
@@ -1,14 +1,16 @@
+import { passwordDTOSchema, usernameDTOSchema } from '#dto/shared/dto-types.js'
 import { Type } from '@sinclair/typebox'
 import Ajv from 'ajv'
 import AddErrors from 'ajv-errors'
-import { passwordDTOSchema, usernameDTOSchema } from '#dto/shared/dto-types.js'
 
 const LoginDTOSchema = Type.Object({
   username: usernameDTOSchema,
   password: passwordDTOSchema
 })
 
-const ajv = new Ajv({ allErrors: true }).addKeyword('kind').addKeyword('modifier')
+const ajv = new Ajv({ allErrors: true })
+  .addKeyword('kind')
+  .addKeyword('modifier')
 ajv.addFormat('password', /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/)
 
 AddErrors(ajv)
