66a25a25733d4622f6d19443a0a6ecac4e09b001
MiguelBarreraDev <barreram987@gmail.com>
2022-12-23 20:10:43 -0500

    Add: Logout action

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,26 @@
+import { Box, Button, Container, Heading } from '@chakra-ui/react'
+import { logoutService } from '../services/privates.service'
+
 export const Home: React.FC = () => {
-  return <div>Home</div>
+  const handleLogout = async (): Promise<void> => {
+    await logoutService()
+  }
+
+  return (
+    <Box w="100%">
+      <Container maxW="8xl" px={2}>
+        <Heading textAlign="center" size={'xl'}>
+          Home
+        </Heading>
+        <Button
+          onClick={handleLogout}
+          colorScheme="gray"
+          variant="solid"
+          shadow="md"
+        >
+          Log out
+        </Button>
+      </Container>
+    </Box>
+  )
 }
