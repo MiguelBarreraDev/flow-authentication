1c6f2ea48cb986768b7b3a68757899a8c6ead124
MiguelBarreraDev <barreram987@gmail.com>
2022-12-27 13:42:39 -0500

    Add: HoC

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ProviderApp } from './ProviderApp'
 import { UserContextProvider } from './context'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <ProviderApp renderApp={<App />} />
     </UserContextProvider>
   </React.StrictMode>
 )
